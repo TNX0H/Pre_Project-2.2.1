adf623b34bba1e44859e46c21f16b2b3bb5ebb77
TNX0H <dimanmityan@yandex.ru>
2022-08-09 23:19:26 +0300

    2.2.1

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +11,6 @@ import java.util.List;
 public class UserServiceImp implements UserService {
 
    private UserDao userDao;
-
-   @Autowired
 
    public UserServiceImp(UserDao userDao) {
       this.userDao = userDao;
